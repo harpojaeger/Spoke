2e333c63b7fb7e363714f6aab5070639ab1af239
Harpo Jaeger <harpo.jaeger@gmail.com>
2019-02-24 19:24:15 -0500

    Add linting functionality to Danger.

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,13 @@
-import { message, danger } from 'danger'
+import { message, danger, markdown } from 'danger'
+import fs from 'fs'
 
 const modifiedMD = danger.git.modified_files.join('- ')
 message('Changed Files in this PR: \n - ' + modifiedMD)
+
+
+const linterOutput = fs.readFileSync('/tmp/eslint.out').toString()
+
+if (linterOutput.includes('Failed')) {
+  markdown(`These changes failed to pass the linter:
+    ${linterOutput}`)
+}
