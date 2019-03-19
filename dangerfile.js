1269fde4c547a4342b63f40b6e2b2c31def68997
Harpo Jaeger <harpo.jaeger@gmail.com>
2019-03-18 21:39:47 -0400

    update travis and danger configs for more helpful linting results

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,30 @@
-import { message, danger, markdown } from 'danger'
-import fs from 'fs'
+import { markdown, danger, fail, warn } from 'danger'
+import { CLIEngine } from 'eslint'
+import config from './.eslintrc'
 
-const modifiedMD = danger.git.modified_files.join('- ')
-message('Changed Files in this PR: \n - ' + modifiedMD)
+const { modified_files: modified, created_files: created } = danger.git
+const linter = new CLIEngine(config)
+const lintReport = linter.executeOnFiles([...modified, ...created])
+// console.log(lintReport.results)
 
-
-const linterOutput = fs.readFileSync('/tmp/eslint.out').toString()
-
-if (linterOutput.includes('problems')) {
-  markdown(`These changes failed to pass the linter:
-    ${linterOutput}`)
+const { warningCount, errorCount } = lintReport
+const resultsTableHeader = '|File|Errors|Warnings\r|---|---|---|\r'
+const resultsTableRows = lintReport.results.map(file =>
+    `|${file.filePath}|${file.errorCount}|${file.warningCount}|`)
+const resultsTable = resultsTableHeader + resultsTableRows.join('\r')
+if (errorCount > 0) {
+  fail(`Linter found ${errorCount} errors and ${warningCount} warnings:
+    ${resultsTable}`)
+} else if (warningCount > 0) {
+  warn(`Linter found ${warningCount} warnings:
+    ${resultsTable}`)
+} else {
+  markdown('This PR passed the linter!')
 }
+
+// const linterOutput = fs.readFileSync('/tmp/eslint.out').toString()
+
+// if (linterOutput.includes('problems')) {
+//   markdown(`These changes failed to pass the linter:
+//     ${linterOutput}`)
+// }
